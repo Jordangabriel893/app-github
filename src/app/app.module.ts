19becebf297eaf97281fc7aacc14e5b28ae54e50
jordangabriel893 <jordan.gabrielbc@gmail.com>
2023-02-09 23:07:57 -0300

    Base do Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule
